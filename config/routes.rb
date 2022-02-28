19d02fa5ff00c66e584b4ce5c6519848872f5cbb
Alexandre Morey <alexander006_9@hotmail.com>
2022-02-28 16:36:16 +0000

    routes done

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :pools, except: :index do
+    resources :bookings, only: [ :create ]
+  end
+  resources :bookings, only: [:index, :destroy, :update, :show] do
+    resources :reviews, only: [:create, :new]
+  end
 end
