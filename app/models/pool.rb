5bfea6167cea3072dd95681248f8792bdd5ecd3e
Alexandre Morey <alexander006_9@hotmail.com>
2022-02-28 15:16:29 +0000

    seed file

--- app/models/pool.rb
+++ app/models/pool.rb
@@ -1,10 +1,9 @@
 class Pool < ApplicationRecord
   belongs_to :user
   has_many :users, through: :bookings
-  
+
   has_many :bookings, dependent: :destroy
 
   validates :price, presence: true
   validates :address, :name, :size, presence: true
-  validates :size, presence: true, uniqueness: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,46 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts "Deleting Pools"
+Pool.destroy_all
+
+puts "Deleting Users"
+User.destroy_all
+
+
+emails = ['p@gmail.com', 'j@gmail.com', 'a@gmail.com', 'f@gmail.com']
+passwords = ['p12345', 'j12345', 'a12345', 'f12345']
+names = ['pierre', 'jonas', 'alex', 'felix']
+addresses = ['bordeaux', 'lille', 'lyon', 'paris']
+
+puts "creating user"
+
+for i in 0..3
+    User.create!(
+        email: emails[i],
+        password: passwords[i],
+        name: names[i]
+    )
+#   puts "user #{user.id} saved"  
+end
+
+array_users_id = []
+User.all.each do |user|
+    array_users_id << user.id
+end
+
+puts 'Creating 100 fake pools...'
+100.times do
+  pool = Pool.new(
+    name:    Faker::Company.name,
+    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    size:  ['small', 'mdeium', 'large'].sample, 
+    price: rand(0..100),
+    user_id: array_users_id.sample
+  )
+  pool.save!
+  puts "pool #{pool.id} saved"
+end
+puts 'Finished!'
