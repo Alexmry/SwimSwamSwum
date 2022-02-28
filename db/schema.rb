e12ba8b4996a1dc8b3872c829b9918916e5a02d0
Alexandre Morey <alexander006_9@hotmail.com>
2022-02-28 11:20:40 +0000

    added the pool model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_28_104844) do
+ActiveRecord::Schema.define(version: 2022_02_28_111947) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pools", force: :cascade do |t|
+    t.string "size"
+    t.string "address"
+    t.float "price"
+    t.string "name"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_pools_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +38,5 @@ ActiveRecord::Schema.define(version: 2022_02_28_104844) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "pools", "users"
 end
