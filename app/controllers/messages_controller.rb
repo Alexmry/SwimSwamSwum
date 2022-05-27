6545f2558632267cb12d9e2ff87eb95ac0be82a4
Alexandre Morey <alexander006_9@hotmail.com>
2022-05-27 11:06:36 +0100

    chatroom

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,25 @@
 class MessagesController < ApplicationController
+    def create
+        @chatroom=Chatroom.find(params[:chatroom_id])
+        # message needs info from the form
+        @message=Message.new(message_params)
+        #message needs a chatroom
+        @message.chatroom = @chatroom
+        #message needs a user
+        @message.user = current_user
+
+        if @message.save
+            redirect_to chatroom_path(@chatroom, anchor: "message-#{@message.id}")
+        else
+            render "chatrooms/show"
+        end
+        binding.pry
+
+    end
+
+    private
+
+    def message_params
+        params.require(:message).permit(:content)
+    end
 end
